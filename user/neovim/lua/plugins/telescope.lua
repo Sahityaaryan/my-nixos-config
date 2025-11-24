c936096a91813381a120cdabdc7154df2ff3a9cb
sahitya <sahityaaryansingh@gmail.com>
2025-11-25 03:26:32 +0530

    fix: fixed the plugins not installed issue by conbiling the installing and loading of the plugins in the same file

--- user/neovim/lua/plugins/telescope.lua
+++ user/neovim/lua/plugins/telescope.lua
@@ -1,36 +1,55 @@
 return {
   "nvim-telescope/telescope.nvim",
   event = "VeryLazy",
-  tag = "0.1.4",
+  tag = "0.1.5",
+  -- 1. WE LIST ALL DEPENDENCIES HERE (Combined from your other file)
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    "debugloop/telescope-undo.nvim",
+    "folke/noice.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
+    "AckslD/nvim-neoclip.lua",
+    { 
+      "nvim-telescope/telescope-fzf-native.nvim", 
+      build = "make",
+      cond = function() return vim.fn.executable("make") == 1 end 
+    },
+    "nvim-tree/nvim-web-devicons",
   },
+  
   config = function()
+    -- 2. SETUP VARIABLES
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-    vim.keymap.set(
-      "n",
-      "<leader>fg",
-      "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-      { desc = "Live Grep" }
-    )
-    vim.keymap.set(
-      "n",
-      "<leader>fc",
-      '<cmd>lua require("telescope.builtin").live_grep({ glob_pattern = "!{spec,test}"})<CR>',
-      { desc = "Live Grep Code" }
-    )
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help Tags" })
+    local telescope = require("telescope")
+    local telescopeConfig = require("telescope.config")
+    local actions = require("telescope.actions")
+    
+    -- Fix for the crash: Require this SAFELY inside the function
+    local undo_actions = require("telescope-undo.actions")
+
+    -- 3. COMBINED KEYMAPS
+    
+    -- A. Kickstart Defaults (The ones from init.lua)
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+    vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+    vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+    vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+    
+    -- B. Your Custom Keymaps (The ones from your file)
+    vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = "Live Grep Args" })
+    vim.keymap.set("n", "<leader>fc", '<cmd>lua require("telescope.builtin").live_grep({ glob_pattern = "!{spec,test}"})<CR>', { desc = "Live Grep Code" })
     vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find Symbols" })
-    vim.keymap.set("n", "<leader>fi", "<cmd>AdvancedGitSearch<CR>", { desc = "AdvancedGitSearch" })
-    vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find Old Files" })
-    vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find Word under Cursor" })
     vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Search Git Commits" })
-    vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, { desc = "Search Git Commits for Buffer" })
-    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })		
+    
+    -- Fuzzy search in current buffer
     vim.keymap.set("n", "<leader>/", function()
-      -- You can pass additional configuration to telescope to change theme, layout, etc.
       require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
         winblend = 10,
         previewer = false,
@@ -38,93 +57,64 @@ return {
       }))
     end, { desc = "[/] Fuzzily search in current buffer" })
 
-    local telescope = require("telescope")
-    local telescopeConfig = require("telescope.config")
-
-    -- Clone the default Telescope configuration
+    -- 4. CUSTOM SETTINGS (Hidden files, etc)
     local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
-
-    -- I want to search in hidden/dot files.
     table.insert(vimgrep_arguments, "--hidden")
-    -- I don't want to search in the `.git` directory.
     table.insert(vimgrep_arguments, "--glob")
     table.insert(vimgrep_arguments, "!**/.git/*")
 
-    local actions = require("telescope.actions")
-
-    local select_one_or_multi = function(prompt_bufnr)
-      local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-      local multi = picker:get_multi_selection()
-      if not vim.tbl_isempty(multi) then
-        require("telescope.actions").close(prompt_bufnr)
-        for _, j in pairs(multi) do
-          if j.path ~= nil then
-            vim.cmd(string.format("%s %s", "edit", j.path))
-          end
-        end
-      else
-        require("telescope.actions").select_default(prompt_bufnr)
-      end
-    end
-
+    -- 5. SETUP TELESCOPE
     telescope.setup({
       defaults = {
-        -- `hidden = true` is not supported in text grep commands.
         vimgrep_arguments = vimgrep_arguments,
         path_display = { "truncate" },
         mappings = {
-          n = {
-            ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
           i = {
             ["<C-j>"] = actions.cycle_history_next,
             ["<C-k>"] = actions.cycle_history_prev,
-            ["<CR>"] = select_one_or_multi,
             ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            ["<C-D>"] = actions.delete_buffer,
-			["<C-s>"] = actions.cycle_previewers_next,
-			["<C-a>"] = actions.cycle_previewers_prev,
+            -- Your custom select logic
+            ["<CR>"] = function(prompt_bufnr)
+               local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+               local multi = picker:get_multi_selection()
+               if not vim.tbl_isempty(multi) then
+                 require("telescope.actions").close(prompt_bufnr)
+                 for _, j in pairs(multi) do
+                   if j.path ~= nil then
+                     vim.cmd(string.format("%s %s", "edit", j.path))
+                   end
+                 end
+               else
+                 require("telescope.actions").select_default(prompt_bufnr)
+               end
+            end,
           },
         },
       },
-      pickers = {
-        find_files = {
-          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-        },
-      },
       extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
+        },
         undo = {
           use_delta = true,
-          use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
-          side_by_side = false,
-          vim_diff_opts = { ctxlen = vim.o.scrolloff },
-          entry_format = "state #$ID, $STAT, $TIME",
           mappings = {
             i = {
-              ["<C-cr>"] = require("telescope-undo.actions").yank_additions,
-              ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
-              ["<cr>"] = require("telescope-undo.actions").restore,
+              ["<C-cr>"] = undo_actions.yank_additions,
+              ["<S-cr>"] = undo_actions.yank_deletions,
+              ["<cr>"] = undo_actions.restore,
             },
           },
         },
       },
     })
 
-    require("telescope").load_extension("neoclip")
-
-    require("telescope").load_extension("fzf")
-
-    require("telescope").load_extension("ui-select")
-    vim.g.zoxide_use_select = true
-
-    require("telescope").load_extension("undo")
-
-    require("telescope").load_extension("advanced_git_search")
-
-    require("telescope").load_extension("live_grep_args")
-
-    require("telescope").load_extension("noice")
+    -- 6. LOAD EXTENSIONS SAFELY
+    pcall(telescope.load_extension, "fzf")
+    pcall(telescope.load_extension, "ui-select")
+    pcall(telescope.load_extension, "neoclip")
+    pcall(telescope.load_extension, "undo")
+    pcall(telescope.load_extension, "live_grep_args")
+    pcall(telescope.load_extension, "noice")
+    -- pcall(telescope.load_extension, "advanced_git_search") -- Uncomment if installed
   end,
 }
-
