2895224670a01df3463f3ae534f46b521c3ef486
sahitya <sahityaaryansingh@gmail.com>
2026-02-03 00:18:28 +0530

    feat: added alias for codium

--- user/sh.nix
+++ user/sh.nix
@@ -5,12 +5,15 @@ let
       ll = "ls -l";
       ".." = "cd ..";
       cls = "clear";
+      nv = "nvim";
+      code = "codium";
       go = "cd";
       rebuild-home-manager = "cd ~/.dotfiles && home-manager switch --flake .#sahitya-nixos-user";
       rebuild-system = "cd ~/.dotfiles && sudo nixos-rebuild switch --flake .#sahitya-nixos";
       go-ll = "cd ~/Desktop/my-space/learning/learning/";
       go-l = "cd ~/Desktop/my-space/learning/";
       go-config = "cd ~/.dotfiles";
+      go-code = "cd ~/Desktop/my-space/coding && codium .";
       go-config-user = "cd ~/.dotfiles/user";
       go-config-system= "cd ~/.dotfiles/system";
       nvi-config = "cd ~/.dotfiles && nvim .";
